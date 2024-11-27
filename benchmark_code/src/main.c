f064e630894b7bc306566a4d660b3ea7ea3f8cc3
Simon Van de Vyver <simon.vandevyver@ugent.be>
2024-11-27 16:00:16 +0100

    add support for compression

--- benchmark_code/src/main.c
+++ benchmark_code/src/main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
+#include <math.h> 
 
 #include "bitpacking.h"
 #include "libsais16x64.h"
@@ -11,7 +12,7 @@
 
 
 void print_help() {
-    printf("Usage: ./libsais64 <sparseness> <optimized> <input_file> <output_file> <dna>\n\n");
+    printf("Usage: ./libsais64 <sparseness> <optimized> <input_file> <output_file> <dna> <compressed>\n\n");
     printf("<sparseness>       : Defines the sparseness factor (an integer).\n");
     printf("<optimized>        : Flag to specify whether the optimized algorithm should be used.\n");
     printf("                     Accepts values: >0 (optimized) or 0 (default).\n");
@@ -19,6 +20,8 @@ void print_help() {
     printf("<output_file>      : The path where the output will be saved.\n");
     printf("<dna>              : Flag to specify whether the input file is DNA data or protein data.\n");
     printf("                     Accepts values: >0 (dna) or 0 (protein).\n");
+    printf("<compressed>       : Flag to specify whether the output SA is compressed by bitpacking.\n");
+    printf("                     Accepts values: >0 (compressed) or 0 (uncompressed).\n");
 }
 
 uint8_t* read_text(char* input_fn, size_t* length) {
@@ -136,7 +139,51 @@ int64_t* build_sa(uint8_t* text, size_t length, int64_t sparseness_factor) {
     return sa;
 }
 
-void write_sa(char* output_fn, int64_t* sa, size_t sa_length) {
+void compress_sa(uint64_t* sa, size_t* sa_length, uint8_t bits_per_element) {
+
+    int64_t element = 0;
+    int8_t shift_element = 64 - bits_per_element;
+    size_t compressed_i = 0;
+    for (size_t i = 0; i < *sa_length; i ++) {
+        if (shift_element < 0) { // new element does not fit in element
+            element |= sa[i] >> (-1 * shift_element);
+            sa[compressed_i] = element;
+            compressed_i ++;
+            element = 0;
+            shift_element += 64;
+        }
+        element |= sa[i] << shift_element;
+        shift_element -= bits_per_element;
+    }
+
+    sa[compressed_i] = element;
+
+    *sa_length = compressed_i + 1;
+}
+
+
+uint64_t* decompress_sa(uint64_t* sa, size_t orig_sa_length, uint8_t bits_per_element) {
+    uint64_t*  decompressed_sa = malloc(orig_sa_length * sizeof(int64_t));
+    
+    int8_t start_shift_element = 0;
+    size_t compressed_i = 0;
+    for (size_t i = 0; i < orig_sa_length; i ++) {
+        decompressed_sa[i] |= (sa[compressed_i] << start_shift_element) >> (64 - bits_per_element);
+        start_shift_element += bits_per_element;
+        
+        if (start_shift_element >= 64) {
+            compressed_i ++;
+            start_shift_element -= 64;
+            if (start_shift_element > 0) { // new element does not fit in element
+                decompressed_sa[i] |= sa[compressed_i] >> (64 - start_shift_element);
+            }
+        }
+    }
+
+    return decompressed_sa;
+}
+
+void write_sa(char* output_fn, uint8_t sparseness_factor, uint64_t* sa, size_t sa_length, int compressed) {
     // Open the output binary file for writing
     FILE *output_file = fopen(output_fn, "wb");
     if (output_file == NULL) {
@@ -146,7 +193,24 @@ void write_sa(char* output_fn, int64_t* sa, size_t sa_length) {
         exit(1);
     }
 
+    // Write bits per element to the binary file
+    uint8_t bits_per_element = 64;
+    if (compressed > 0) {
+        bits_per_element = (uint8_t) log2(sa_length * sparseness_factor) + 1;
+    }
+    fwrite(&bits_per_element, sizeof(uint8_t), 1, output_file);
+
+    // Write sparseness factor to the binary file
+    fwrite(&sparseness_factor, sizeof(uint8_t), 1, output_file);
+
+    // Write length of sa to binary file
+    uint64_t sa_length64 = (uint64_t) sa_length;
+    fwrite(&sa_length64, sizeof(uint64_t), 1, output_file);
+
     // Write the suffix array to the binary file
+    if (compressed > 0) {
+        compress_sa(sa, &sa_length, bits_per_element);
+    }
     fwrite(sa, sizeof(int64_t), sa_length, output_file);
     fclose(output_file);
 }
@@ -158,7 +222,7 @@ int main(int argc, char *argv[]) {
     }
     printf("\n");
 
-    if (argc != 6) {
+    if (argc != 7) {
         print_help();
         return 1;
     }
@@ -166,6 +230,7 @@ int main(int argc, char *argv[]) {
     int64_t sparseness_factor = atoi(argv[1]);
     int dna = atoi(argv[5]);
     int optimized = atoi(argv[2]);
+    int compressed = atoi(argv[6]);
 
     clock_t start_reading = clock();
     printf("Started reading input file...\n");
@@ -182,16 +247,15 @@ int main(int argc, char *argv[]) {
         sa = build_sa_optimized(text, length, sparseness_factor, sa_length, dna);
     } else {
         sa = build_sa(text, length, sparseness_factor);
+        free(text);
     }
     printf("Done building SA in %fs\n", ((double) clock() - start_sa) / CLOCKS_PER_SEC);
 
     clock_t start_writing = clock();
     printf("Started writing results...\n");
-    write_sa(argv[4], sa, sa_length);
+    write_sa(argv[4], (uint8_t) sparseness_factor_size, (uint64_t*) sa, sa_length, compressed);
     printf("Done writing results to %s in %fs\n\n", argv[4], ((double) clock() - start_writing) / CLOCKS_PER_SEC);
 
-    // Clean up
-    free(text);
     return 0;
 }
 
